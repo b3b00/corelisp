2350c6fae228c16df48ad273e23a6795be7ba710
Olivier Duhart <olivier.duhart@sage.com>
2020-04-20 09:57:32 +0200

    atom primitive test

--- CoreLispTests/AtomTests.cs
+++ CoreLispTests/AtomTests.cs
@@ -15,60 +15,73 @@ namespace CoreLispTests
 
 
 
-    [Test]
-    public void IntTest()
-    {
-        var r = Test("1");
-        Assert.IsInstanceOf<IntLiteral>(r);
-        Assert.AreEqual(1, (r as IntLiteral).Value);
-    }
+        [Test]
+        public void IntTest()
+        {
+            var r = Test("1");
+            Assert.IsInstanceOf<IntLiteral>(r);
+            Assert.AreEqual(1, (r as IntLiteral).Value);
+        }
 
-    [Test]
-    public void DoubleTest()
-    {
-        var r = Test("1.28");
-        Assert.IsInstanceOf<DoubleLiteral>(r);
-        Assert.AreEqual(1.28, (r as DoubleLiteral).Value);
-    }
+        [Test]
+        public void DoubleTest()
+        {
+            var r = Test("1.28");
+            Assert.IsInstanceOf<DoubleLiteral>(r);
+            Assert.AreEqual(1.28, (r as DoubleLiteral).Value);
+        }
 
-    [Test]
-    public void StringTest()
-    {
-        var r = Test("\"string\"");
-        Assert.IsInstanceOf<StringLiteral>(r);
-        Assert.AreEqual("string", (r as StringLiteral).Value);
-    }
+        [Test]
+        public void StringTest()
+        {
+            var r = Test("\"string\"");
+            Assert.IsInstanceOf<StringLiteral>(r);
+            Assert.AreEqual("string", (r as StringLiteral).Value);
+        }
 
-    [Test]
-    public void QuotedSymbolTest()
-    {
-        var r = Test("'symbol");
-        Assert.IsInstanceOf<SymbolLiteral>(r);
-        Assert.AreEqual("symbol", (r as SymbolLiteral).Value);
-    }
+        [Test]
+        public void QuotedSymbolTest()
+        {
+            var r = Test("'symbol");
+            Assert.IsInstanceOf<SymbolLiteral>(r);
+            Assert.AreEqual("symbol", (r as SymbolLiteral).Value);
+        }
 
-    [Test]
-    public void EmptySExprTest()
-    {
-        var r = Test("()");
-        Assert.IsInstanceOf<NilLiteral>(r);
-        Assert.False((r as NilLiteral).BooleanValue);
-    }
+        [Test]
+        public void EmptySExprTest()
+        {
+            var r = Test("()");
+            Assert.IsInstanceOf<NilLiteral>(r);
+            Assert.False((r as NilLiteral).BooleanValue);
+        }
 
-    [Test]
-    public void NilTest()
-    {
-        var r = Test("nil");
-        Assert.IsInstanceOf<NilLiteral>(r);
-        Assert.False((r as NilLiteral).BooleanValue);
-    }
+        [Test]
+        public void NilTest()
+        {
+            var r = Test("nil");
+            Assert.IsInstanceOf<NilLiteral>(r);
+            Assert.False((r as NilLiteral).BooleanValue);
+        }
 
-    [Test]
-    public void QuotedTrueTest()
-    {
-        var r = Test("'t");
-        Assert.IsInstanceOf<SymbolLiteral>(r);
-        Assert.True((r as SymbolLiteral).BooleanValue);
-    }
+        [Test]
+        public void QuotedTrueTest()
+        {
+            var r = Test("'t");
+            Assert.IsInstanceOf<SymbolLiteral>(r);
+            Assert.True((r as SymbolLiteral).BooleanValue);
+        }
+        
+        [Test]
+        public void AtomPrimitiveTest()
+        {
+            var r = Test("atom 't");
+            Assert.IsInstanceOf<SymbolLiteral>(r);
+            Assert.True(r.BooleanValue);
+
+            r = Test("atom var");
+            Assert.IsInstanceOf<NilLiteral>(r);
+            Assert.False(r.BooleanValue);
+        }
+        
     }
 }
