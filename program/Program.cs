eb0574a0677320f8cbc24f8b668556c8af4c7d67
Olivier Duhart <olivier.duhart@sage.com>
2020-04-21 11:18:29 +0200

    main corelisp lib

--- program/Program.cs
+++ program/Program.cs
@@ -6,44 +6,25 @@ using lispparser.core.lisp.model;
 using lispparser.core.lisp.parser;
 using sly.parser;
 using sly.parser.generator;
-
+using coreLisp;
 
 namespace program
 {
     class Program
     {
-        private static Parser<LispLexer, ILisp> Parser = null;
+        private static CoreLisp coreLisp = new CoreLisp();
 
-        static void BuildParser()
-        {
-            ParserBuilder<LispLexer, ILisp> builder = new ParserBuilder<LispLexer, ILisp>();
-            var ParserResult = builder.BuildParser(new LispParser(), ParserType.EBNF_LL_RECURSIVE_DESCENT, "root");
-            if (ParserResult.IsOk)
-            {
-                Parser = ParserResult.Result;
-            }
-        }
+       
 
         static void LispInLisp()
         {
             string source = File.ReadAllText("lisp.lisp");
-            BuildParser();
-            var r = Run(source);
+            var r = coreLisp.Run(source);
             ;
         }
 
 
-        static LispLiteral Run(string source,bool debug = false)
-        {
-            var r = Parser.Parse(source);
-            if (r.IsError)
-            {
-                r.Errors.ForEach(e => Console.WriteLine(e.ErrorMessage));
-                return null;
-            }
-            var x = LispInterpreter.LispInterpreter.Interprete(new Context(), r.Result as LispProgram);
-            return x;
-        }
+        
 
 
         
@@ -51,7 +32,7 @@ namespace program
         {
             
             
-            Run(@"
+            coreLisp.Run(@"
 (setq vname 'x )
 (set vname ""vautour"")
 (print ""X"") 
@@ -66,7 +47,6 @@ namespace program
 
         static void Main(string[] args)
         {
-            BuildParser();
             LispInLisp();
         }
     }
