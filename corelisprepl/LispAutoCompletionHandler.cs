a1f66683143ea2abd9b72ff3579e6187ff150bef
Olivier Duhart <olivier.duhart@sage.com>
2020-04-24 11:33:47 +0200

    better autocompletion

--- corelisprepl/LispAutoCompletionHandler.cs
+++ corelisprepl/LispAutoCompletionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using coreLisp;
 
@@ -20,9 +21,25 @@ namespace corelisprepl
         // index - The index of the terminal cursor within {text}
         public string[] GetSuggestions(string text, int index)
         {
+            List<string > commands = new List<string>() {"load", "context", "quit"};
+            commands.AddRange(CoreLisp.Context.Scope.Keys.ToList());
             
-            return CoreLisp.Context.Scope.Keys.ToArray();
+            int i = text.LastIndexOf("(");
+            string start = text;
+            if (i >= 0)
+            {
+                start = text.Substring(i+1);
+            }
             
+            var t =  commands.Where(x => x.StartsWith(start));
+            foreach (var maybe in t)
+            {
+                string mm = text.Substring(0, Math.Max(0,i+1));
+                string prop = mm + maybe;
+                ;
+            }
+                return t.Select(x => text.Substring(0,Math.Max(0,i+1)) + x).ToArray();
+
         }
     }
 }
